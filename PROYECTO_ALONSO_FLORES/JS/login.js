99e76e28bdaa3e03d15515685297ef57f405393b
FuFFu24 <floalonso.abad@gmail.com>
2023-10-06 09:44:25 +0200

    05/10 optimizar y mejorar proyecto

--- PROYECTO_ALONSO_FLORES/JS/login.js
+++ PROYECTO_ALONSO_FLORES/JS/login.js
@@ -1,21 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Obtén los elementos de los formularios y el enlace
+  // Obtén los elementos del formulario de registro, formulario de inicio de sesión y el enlace de mensaje
   var registerForm = document.querySelector(".register-form");
   var loginForm = document.querySelector(".login-form");
-  var messageLink = document.querySelector(".message a");
+  var messageLinkRegister = document.querySelector(".message-register a");
+  var messageLinkLogin = document.querySelector(".message-login a");
 
-  // Agrega un evento de clic al enlace
-  messageLink.addEventListener("click", function (event) {
-    event.preventDefault(); // Evita que el enlace haga clic en su comportamiento predeterminado
-    toggleForms();
+  // Agrega un evento de clic al enlace de mensaje
+  messageLinkRegister.addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que el enlace realice su comportamiento predeterminado
+    toggleForms(); // Llama a la función para alternar entre los formularios
+  });
+
+  // Agrega un evento de clic al enlace de mensaje
+  messageLinkLogin.addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que el enlace realice su comportamiento predeterminado
+    toggleForms(); // Llama a la función para alternar entre los formularios
   });
 
   // Función para alternar la visibilidad de los formularios
   function toggleForms() {
     if (registerForm.style.display === "block") {
+      // Si el formulario de registro se muestra, ocúltalo y muestra el formulario de inicio de sesión
       registerForm.style.display = "none";
       loginForm.style.display = "block";
     } else {
+      // Si el formulario de inicio de sesión se muestra, ocúltalo y muestra el formulario de registro
       registerForm.style.display = "block";
       loginForm.style.display = "none";
     }
