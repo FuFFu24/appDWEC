21a7f36676683333d4d097597d0a78cee2629ef6
FuFFu24 <floalonso.abad@gmail.com>
2023-10-09 14:17:06 +0200

    09/10 clases proyecto

--- PROYECTO_ALONSO_FLORES/JS/scripts.js
+++ PROYECTO_ALONSO_FLORES/JS/scripts.js
@@ -1,5 +1,5 @@
-/* document.addEventListener("DOMContentLoaded", function () {
-  // Obtén el icono de usuario y el desplegable
+document.addEventListener("DOMContentLoaded", function () {
+  /* // Obtén el icono de usuario y el desplegable
   const userIcon = document.getElementById("user-icon");
   const userDropdown = document.getElementById("user-dropdown");
 
@@ -17,6 +17,62 @@
     ) {
       userDropdown.classList.remove("show");
     }
-  });
+  }); */
+
+  var listaJuegosMesa = [];
+
+  function cargarJuegosMesa() {
+    $.getJSON("../JSON/productoJSON.txt", function (datos) {
+      listaJuegosMesa = datos;
+      mostrarJuegosMesa(listaJuegosMesa);
+    });
+  }
+
+  cargarJuegosMesa();
+
+  // Función para crear el HTML de un producto
+  function mostrarJuegosMesa(lista) {
+    const destacados = document.querySelector(".destacados");
+    destacados.innerHTML = "<table></table>";
+    var tbody = destacados.getElementsByTagName("table")[0];
+    lista.forEach((producto,index) => {
+      if (index % 5 == 0) {
+        // Crear una nueva fila de productos
+        tbody.innerHTML += `<tr>
+        <td>
+                <div class="juego-destacado">
+                <a href="#" class="web-page">
+                <div class="img-juego">
+                  <img src="${producto.imagenURL}" alt="${producto.nombre}" />
+                </div>
+                <div class="datos-juego">
+                  <p>${producto.nombre}</p>
+                  <h3>${producto.precio} €</h3>
+                </div>
+              </a>
+              <a href="#" class="btn-anadir-carrito">Añadir al carrito</a>
+              </div>
+            </td>
+            </tr>`;
+    } else {
+      tbody.innerHTML += `
+                <td>
+                <div class="juego-destacado">
+                <a href="#" class="web-page">
+                <div class="img-juego">
+                  <img src="${producto.imagenURL}" alt="${producto.nombre}" />
+                </div>
+                <div class="datos-juego">
+                  <p>${producto.nombre}</p>
+                  <h3>${producto.precio} €</h3>
+                </div>
+              </a>
+              <a href="#" class="btn-anadir-carrito">Añadir al carrito</a>
+              </div>
+            </td>`;
+    }
+      
+    });
+
+  }
 });
- */
