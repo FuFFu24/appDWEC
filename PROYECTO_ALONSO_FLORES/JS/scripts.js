f6a44b50988800faecafc7dd69bc530ad4cd32d1
FuFFu24 <floalonso.abad@gmail.com>
2023-10-11 12:30:33 +0200

    11/10

--- PROYECTO_ALONSO_FLORES/JS/scripts.js
+++ PROYECTO_ALONSO_FLORES/JS/scripts.js
@@ -24,6 +24,11 @@ document.addEventListener("DOMContentLoaded", function () {
   function cargarJuegosMesa() {
     $.getJSON("../JSON/productoJSON.txt", function (datos) {
       listaJuegosMesa = datos;
+      ordenarPorDescuento(listaJuegosMesa);
+      mostrarJuegosMesa(listaJuegosMesa);
+      ordenarPorFecha(listaJuegosMesa);
+      mostrarJuegosMesa(listaJuegosMesa);
+      ordenarPorNota(listaJuegosMesa);
       mostrarJuegosMesa(listaJuegosMesa);
     });
   }
@@ -32,47 +37,81 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Función para crear el HTML de un producto
   function mostrarJuegosMesa(lista) {
-    const destacados = document.querySelector(".destacados");
-    destacados.innerHTML = "<table></table>";
-    var tbody = destacados.getElementsByTagName("table")[0];
-    lista.forEach((producto,index) => {
-      if (index % 5 == 0) {
-        // Crear una nueva fila de productos
-        tbody.innerHTML += `<tr>
-        <td>
-                <div class="juego-destacado">
-                <a href="#" class="web-page">
-                <div class="img-juego">
-                  <img src="${producto.imagenURL}" alt="${producto.nombre}" />
-                </div>
-                <div class="datos-juego">
-                  <p>${producto.nombre}</p>
-                  <h3>${producto.precio} €</h3>
-                </div>
-              </a>
-              <a href="#" class="btn-anadir-carrito">Añadir al carrito</a>
-              </div>
-            </td>
-            </tr>`;
-    } else {
-      tbody.innerHTML += `
-                <td>
-                <div class="juego-destacado">
-                <a href="#" class="web-page">
-                <div class="img-juego">
-                  <img src="${producto.imagenURL}" alt="${producto.nombre}" />
-                </div>
-                <div class="datos-juego">
-                  <p>${producto.nombre}</p>
-                  <h3>${producto.precio} €</h3>
-                </div>
-              </a>
-              <a href="#" class="btn-anadir-carrito">Añadir al carrito</a>
-              </div>
-            </td>`;
-    }
-      
+    const productGrid = document.querySelector(".product-grid");
+
+    lista.forEach((producto, index) => {
+      if (index >= 5) {
+        return; // Sal del bucle si se han procesado 5 elementos
+      }
+
+      // Crea un nuevo juego destacado
+      const juegoDestacado = document.createElement("div");
+      juegoDestacado.className = "juego-destacado";
+
+      // Crea el contenido del juego destacado
+      const contenidoHTML = `
+        <a href="#" class="web-page">
+          <div class="img-juego">
+            <img src="${producto.imagenURL}" alt="${producto.nombre}" />
+          </div>
+          <div class="datos-juego">
+            <p>${producto.nombre}</p>
+            <h3>${producto.precio} €</h3>
+          </div>
+        </a>
+        <a href="#" class="btn-anadir-carrito">Añadir al carrito</a>
+      `;
+
+      // Agrega el contenido al juego destacado
+      juegoDestacado.innerHTML = contenidoHTML;
+
+      // Agrega el juego destacado al contenedor .product-grid
+      productGrid.appendChild(juegoDestacado);
+    });
+  }
+
+  // Función para ordenar la lista por la propiedad "nota"
+  function ordenarPorNota(lista) {
+    lista.sort((a, b) => {
+      const notaA = parseFloat(a.nota);
+      const notaB = parseFloat(b.nota);
+      return notaB - notaA; // Ordena de mayor a menor nota
     });
 
+    const destacados = document.querySelector(".mejor-valorados");
+    destacados.innerHTML =
+      '<h2>Juegos mejor valorados</h2><div class="product-grid"></div>';
+  }
+
+  // Función para ordenar la lista por la propiedad "fecha"
+  function ordenarPorFecha(lista) {
+    lista.sort((a, b) => {
+      const fechaA = new Date(b.fecha);
+      const fechaB = new Date(a.fecha);
+      return fechaA - fechaB;
+    });
+
+    const destacados = document.querySelector(".novedades");
+    destacados.innerHTML =
+      '<h2>Destacados</h2><div class="product-grid"></div>';
+  }
+
+  // Función para ordenar la lista por la propiedad "descuento"
+  function ordenarPorDescuento(lista) {
+    lista.sort((a, b) => {
+      // Compara el atributo "descuento"
+      if (a.descuento && !b.descuento) {
+        return -1; // a viene primero (tiene descuento)
+      } else if (!a.descuento && b.descuento) {
+        return 1; // b viene primero (tiene descuento)
+      } else {
+        // Si tienen el mismo estado de "descuento", compara "porcentajeDescuento"
+        return b.porcentajeDescuento - a.porcentajeDescuento;
+      }
+    });
+
+    const destacados = document.querySelector(".en-oferta");
+    destacados.innerHTML =
+      '<h2>Juegos en oferta</h2><div class="product-grid"></div>';
   }
 });
