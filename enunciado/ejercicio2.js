6a141c7f374a34fc2bd9dcf0731f835e3c306cc8
FuFFu24 <floalonso.abad@gmail.com>
2023-11-28 23:09:03 +0100

    28/11 repaso

--- enunciado/ejercicio2.js
+++ enunciado/ejercicio2.js
@@ -1,239 +1,226 @@
-const listaVinilos = [
-  "25996_1.jpg",
-  "25996_2.jpg",
-  "25997_1.jpg",
-  "25997_3.jpg",
-  "26650_3.jpg",
-  "26704_3.jpg",
-  "26868_1.jpg",
-  "26868_2.jpg",
-  "26869_1.jpg",
-  "26869_2.jpg",
-  "26873_1.jpg",
-];
-const listaLaminas = [
-  "26873_2.jpg",
-  "27095_3.jpg",
-  "27356_3.jpg",
-  "27429_1.jpg",
-  "27429_2.jpg",
-  "27773_3.jpg",
-  "28155_1.jpg",
-  "28155_2.jpg",
-  "28192_3.jpg",
-  "28192_4.jpg",
-  "29776_3.jpg",
-];
-const listaOtros = [
-  "30390_3.jpg",
-  "30473_1.jpg",
-  "31386_1.jpg",
-  "31386_2.jpg",
-  "31469_3.jpg",
-];
-const listaTodos = [...listaVinilos, ...listaLaminas, ...listaOtros];
+document.addEventListener("DOMContentLoaded", function () {
+  const listaVinilos = [
+    "25996_1.jpg",
+    "25996_2.jpg",
+    "25997_1.jpg",
+    "25997_3.jpg",
+    "26650_3.jpg",
+    "26704_3.jpg",
+    "26868_1.jpg",
+    "26868_2.jpg",
+    "26869_1.jpg",
+    "26869_2.jpg",
+    "26873_1.jpg",
+  ];
+  const listaLaminas = [
+    "26873_2.jpg",
+    "27095_3.jpg",
+    "27356_3.jpg",
+    "27429_1.jpg",
+    "27429_2.jpg",
+    "27773_3.jpg",
+    "28155_1.jpg",
+    "28155_2.jpg",
+    "28192_3.jpg",
+    "28192_4.jpg",
+    "29776_3.jpg",
+  ];
+  const listaOtros = [
+    "30390_3.jpg",
+    "30473_1.jpg",
+    "31386_1.jpg",
+    "31386_2.jpg",
+    "31469_3.jpg",
+  ];
+  const listaTodos = [...listaVinilos, ...listaLaminas, ...listaOtros];
 
-const compras = [];
-const alquileres = [];
+  const divExposicion = document.getElementById("exposicion");
 
-function generarOptions() {
-  let opciones = "";
-  for (let i = 1; i <= 20; i++) {
-    opciones += `<option>${i}</option>\n`;
+  function mostrarTodosCuadros(lista) {
+    divExposicion.innerHTML = "";
+    const tabla = document.createElement("table");
+    let tr;
+
+    lista.forEach((element, index) => {
+      if (index % 4 === 0) {
+        tr = document.createElement("tr");
+        tabla.appendChild(tr);
+      }
+
+      const td = document.createElement("td");
+      td.innerHTML = `
+          <img src="pagina2_files/${element}" alt="...">
+        <br>
+          <button type="button" class="contratar">CONTRATAR</button>
+          <button type="button" class="alquilar">ALQUILAR</button>
+        <br>
+          <label for="cantidad">CANTIDAD:</label>
+          <input type="number" name="cantidad" id="cantidad">
+        <br>
+          <label for="duracion">DURACION:</label>
+          <select name="duracion" id="duracion">
+            <option value="Escoge">Escoge</option>
+            ${valoresSelect()}
+          </select>
+      `;
+      tr.appendChild(td);
+
+      const btnContratar = td.querySelector(".contratar");
+      const btnAlquilar = td.querySelector(".alquilar");
+
+      btnContratar.addEventListener("click", function (event) {
+        const imagen = element;
+        const cantidad =
+          event.target.nextSibling.nextSibling.nextSibling.nextSibling
+            .nextSibling.nextSibling.nextSibling.nextSibling.value;
+        const operacion = event.target.innerText;
+
+        if (imagen && cantidad) {
+          if (!isNaN(cantidad) && cantidad > 0) {
+            let comprasJSON = localStorage.getItem("compras");
+            let compras = JSON.parse(comprasJSON) || [];
+
+            let nuevaCompra = {
+              foto: imagen,
+              cantidad: cantidad,
+              operacion: operacion,
+            };
+
+            compras.push(nuevaCompra);
+
+            localStorage.setItem("compras", JSON.stringify(compras));
+
+            const cantidadCaja =
+              event.target.nextSibling.nextSibling.nextSibling.nextSibling
+                .nextSibling.nextSibling.nextSibling.nextSibling;
+
+            cantidadCaja.style.display = "none";
+
+            btnContratar.disabled = "true";
+          }
+        }
+      });
+
+      btnAlquilar.addEventListener("click", function (event) {
+        const imagen = element;
+        const cantidad =
+          event.target.nextSibling.nextSibling.nextSibling.nextSibling
+            .nextSibling.nextSibling.value;
+        const duracion =
+          event.target.nextSibling.nextSibling.nextSibling.nextSibling
+            .nextSibling.nextSibling.nextSibling.nextSibling.nextSibling
+            .nextSibling.nextSibling.nextSibling.value;
+        const operacion = event.target.innerText;
+
+        if (imagen && cantidad && duracion) {
+          if (!isNaN(cantidad) && cantidad > 0 && duracion !== "Escoge") {
+            let comprasJSON = localStorage.getItem("compras");
+            let compras = JSON.parse(comprasJSON) || [];
+
+            let nuevaCompra = {
+              foto: imagen,
+              cantidad: cantidad,
+              duracion: duracion,
+              operacion: operacion,
+            };
+
+            compras.push(nuevaCompra);
+
+            localStorage.setItem("compras", JSON.stringify(compras));
+
+            const td = event.target.parentNode;
+
+            while (td.firstChild) {
+              td.removeChild(td.firstChild);
+            }
+          }
+        }
+      });
+    });
+
+    divExposicion.appendChild(tabla);
   }
-  return opciones;
-}
 
-function contratar(button) {
-  const cantidadInput =
-    button.nextElementSibling.nextElementSibling.nextElementSibling;
-  const cantidad = cantidadInput.value;
-
-  if (!isNaN(cantidad) && cantidad > 0) {
-    const cuadro = button.previousElementSibling.previousElementSibling;
-    const fotoLinkEntero = cuadro.src;
-    const foto = fotoLinkEntero.replace(
-      "http://localhost:8080/enunciado/pagina2_files/",
-      ""
-    );
-
-    guardarCompra(foto, cantidad, "CONTRATAR");
-
-    cantidadInput.style.display = "none";
-
-    button.disabled = true;
-  }
-}
-
-function alquilar(button) {
-  const cantidadInput = button.nextElementSibling;
-  const cantidad = parseInt(cantidadInput.value, 10);
-  const duracionSelect =
-    button.nextElementSibling.nextElementSibling.nextElementSibling
-      .nextElementSibling;
-  const duracion = duracionSelect.value;
-
-  if (!isNaN(cantidad) && cantidad > 0 && duracion != "Escoge") {
-    const cuadro =
-      button.previousElementSibling.previousElementSibling
-        .previousElementSibling.previousElementSibling;
-    const fotoLinkEntero = cuadro.src;
-    const foto = fotoLinkEntero.replace(
-      "http://localhost:8080/enunciado/pagina2_files/",
-      ""
-    );
-
-    guardarAlquiler(foto, cantidad, duracion, "ALQUILAR");
-
-    const trEliminar = cantidadInput.parentNode;
-
-    trEliminar.style.display = "none";
-  }
-}
-
-function cargarCuadros(lista) {
-  const exposicion = document.getElementById("exposicion");
-  exposicion.innerHTML = "";
-  const tabla = document.createElement("table");
-  tabla.style.border = "2px solid black";
-  let contadorTds = 1;
-
-  let tr = document.createElement("tr");
-
-  for (let i = 0; i < lista.length; i++) {
-    let contenido = `
-            <img src="./pagina2_files/${lista[i]}" alt=""><br>
-            <button type="button" onclick="contratar(this)">CONTRATAR</button><br>
-            <button type="button" onclick="alquilar(this)">ALQUILAR</button>
-            <input type="text" name="cantidad" id="cantidad" style="width: 30px"><br>
-            <label for="duracion">DURACION: </label>
-            <select name="duracion" id="duracion">
-                <option>Escoge</option>
-                ${generarOptions()}
-            </select>
-        `;
-
-    if (contadorTds % 4 === 0) {
-      tr = document.createElement("tr");
+  function valoresSelect() {
+    let options = "";
+    for (let i = 1; i <= 20; i++) {
+      options += `<option value="${i}">${i}</option>`;
     }
-
-    const td = document.createElement("td");
-    td.style.border = "2px solid black";
-    td.innerHTML = contenido;
-    tr.appendChild(td);
-
-    if (contadorTds % 4 === 0 || i === lista.length - 1) {
-      tabla.appendChild(tr);
-    }
-
-    contadorTds++;
+    return options;
   }
 
-  exposicion.appendChild(tabla);
-}
+  mostrarTodosCuadros(listaTodos);
 
-function guardarCompra(foto, cantidad, operacion) {
-  compras.push({ foto, cantidad, operacion });
-}
+  const selectFiltrarCuadros = document.querySelector("#central select");
 
-function guardarAlquiler(foto, cantidad, duracion, operacion) {
-  alquileres.push({ foto, cantidad, duracion, operacion });
-}
-
-function mostrarCompras() {
-  compras.sort((a, b) => b.cantidad - a.cantidad);
-  const ventana = window.open("", "COMPRA", "width=600,height=400");
-  ventana.document.write("<h1>COMPRA</h1>");
-  ventana.document.write("<p>IMAGEN --- OPERACION --- CANTIDAD</p>");
-  for (const compra of compras) {
-    ventana.document.write(
-      `<p>${compra.foto} --- ${compra.operacion} --- ${compra.cantidad}</p>`
-    );
-  }
-  ventana.document.write('<button onclick="window.close()">Cerrar</button>');
-}
-
-function mostrarAlquileres() {
-  alquileres.sort((a, b) => a.duracion - b.duracion);
-  const ventana = window.open("", "ALQUILER", "width=600,height=400");
-  ventana.document.write("<h1>ALQUILER</h1>");
-  ventana.document.write(
-    "<p>IMAGEN --- OPERACION --- CANTIDAD --- DURACION</p>"
-  );
-  for (const alquiler of alquileres) {
-    ventana.document.write(
-      `<p>${alquiler.foto} --- ${alquiler.operacion} --- ${alquiler.cantidad} --- ${alquiler.duracion}</p>`
-    );
-  }
-  ventana.document.write('<button onclick="window.close()">Cerrar</button>');
-}
-
-function mostrarTodasLasOperaciones() {
-  const operaciones = [...compras, ...alquileres];
-  operaciones.sort((a, b) => b.cantidad - a.cantidad);
-  const ventana = window.open(
-    "",
-    "Todas las operaciones",
-    "width=600,height=400"
-  );
-  ventana.document.write("<h1>TODAS LAS OPERACIONES</h1>");
-  ventana.document.write(
-    "<p>IMAGEN --- OPERACION --- CANTIDAD --- DURACION</p>"
-  );
-  for (const operacion of operaciones) {
-    if (compras.find((c) => c.foto === operacion.foto)) {
-      ventana.document.write(
-        `${operacion.foto} --- ${operacion.operacion} --- ${operacion.cantidad}</p>`
-      );
+  selectFiltrarCuadros.addEventListener("change", () => {
+    if (selectFiltrarCuadros.value === "Vinilos") {
+      mostrarTodosCuadros(listaVinilos);
+    } else if (selectFiltrarCuadros.value === "Laminas") {
+      mostrarTodosCuadros(listaLaminas);
+    } else if (selectFiltrarCuadros.value === "Otros") {
+      mostrarTodosCuadros(listaOtros);
     } else {
-      ventana.document.write(
-        `${operacion.foto} --- ${operacion.operacion} --- ${operacion.cantidad} --- ${operacion.duracion}</p>`
-      );
-    }
-  }
-  ventana.document.write('<button onclick="window.close()">Cerrar</button>');
-}
-
-window.onload = function () {
-  cargarCuadros(listaTodos);
-
-  const filtroCuadros = document.querySelector("#central select");
-
-  filtroCuadros.addEventListener("change", function () {
-    const tipoSeleccionado = filtroCuadros.value;
-    if (tipoSeleccionado == "Vinilos") {
-      cargarCuadros(listaVinilos);
-    } else if (tipoSeleccionado == "Laminas") {
-      cargarCuadros(listaLaminas);
-    } else if (tipoSeleccionado == "Otros") {
-      cargarCuadros(listaOtros);
-    } else if (tipoSeleccionado == "Escoge un valor") {
-      cargarCuadros(listaTodos);
+      mostrarTodosCuadros(listaTodos);
     }
   });
 
-  const verCompras = document
-    .querySelector("#derecha")
-    .querySelectorAll("a")[0];
-  const verAlquileres = document
-    .querySelector("#derecha")
-    .querySelectorAll("a")[1];
-  const verOperaciones = document
-    .querySelector("#derecha")
-    .querySelectorAll("a")[2];
+  const verCompras = document.querySelectorAll("#derecha a")[0];
+  const verTodas = document.querySelectorAll("#derecha a")[2];
 
-  verCompras.addEventListener("click", function (event) {
-    event.preventDefault();
-    mostrarCompras();
+  verCompras.addEventListener("click", function () {
+    const ventana = window.open(
+      "compras",
+      "compras",
+      "width= 600px, height= 600px"
+    );
+    let comprasJSON = localStorage.getItem("compras");
+    let compras = JSON.parse(comprasJSON) || [];
+    ventana.document.write(`
+      <h1>COMPRAS</h1>
+      <table>
+      <tr><th>IMAGEN<th><th>OPERACION<th><th>CANTIDAD<th></tr>
+    `);
+    compras.forEach((element) => {
+      if (element.operacion == "CONTRATAR") {
+        ventana.document.write(`
+      <tr><td>${element.foto}<td><td>${element.cantidad}<td><td>${element.operacion}<td></tr>
+    `);
+      }
+    });
+    ventana.document.write(`
+    </table>
+    <button onclick="window.close()">CERRAR</button>
+  `);
   });
 
-  verAlquileres.addEventListener("click", function (event) {
-    event.preventDefault();
-    mostrarAlquileres();
+  verTodas.addEventListener("click", function () {
+    const ventana = window.open(
+      "todas",
+      "todas",
+      "width= 600px, height= 600px"
+    );
+    let comprasJSON = localStorage.getItem("compras");
+    let compras = JSON.parse(comprasJSON) || [];
+    ventana.document.write(`
+      <h1>COMPRAS</h1>
+      <table>
+      <tr><th>IMAGEN<th><th>OPERACION<th><th>DURACION<th><th>CANTIDAD<th></tr>
+    `);
+    compras.forEach((element) => {
+      if (element.duracion) {
+        ventana.document.write(`
+      <tr><td>${element.foto}<td><td>${element.cantidad}<td><td>${element.duracion}<td><td>${element.operacion}<td></tr>
+    `);
+      } else {
+        ventana.document.write(`
+      <tr><td>${element.foto}<td><td>${element.cantidad}<td><td><td><td>${element.operacion}<td></tr>
+    `);
+      }
+    });
+    ventana.document.write(`
+    </table>
+    <button onclick="window.close()">CERRAR</button>
+  `);
   });
-
-  verOperaciones.addEventListener("click", function (event) {
-    event.preventDefault();
-    mostrarTodasLasOperaciones();
-  });
-};
+});
